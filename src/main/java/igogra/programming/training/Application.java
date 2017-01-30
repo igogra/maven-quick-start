cdacb084a06373d69866badc8ee0f7aa78001353
Ivan Gomez <ivan.gomez.grande@gmail.com>
2017-01-30 23:36:59 +0100

    Changing compiler plugin to Java source 1.8

--- src/main/java/igogra/programming/training/Application.java
+++ src/main/java/igogra/programming/training/Application.java
@@ -1,13 +1,26 @@
 package igogra.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println("Inside Application");
+    }
+
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+
+        for (String greeting : greetings) {
+            System.out.println("Greeting: " + greeting);
+        }
     }
 
     public static void main (String[] args) {
-        System.out.println ("Starting Application");
+        System.out.println("Starting Application");
         Application app = new Application();
+        app.greet();
     }
 }
