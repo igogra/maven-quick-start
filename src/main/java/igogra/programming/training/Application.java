f0cdcafe212bf873159347e03468f246e7831869
Ivan Gomez <ivan.gomez.grande@gmail.com>
2017-01-30 23:54:09 +0100

    Use StringUtils and add dependency to lang3

--- src/main/java/igogra/programming/training/Application.java
+++ src/main/java/igogra/programming/training/Application.java
@@ -2,6 +2,7 @@ package igogra.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
@@ -18,9 +19,16 @@ public class Application {
         }
     }
 
+    public int countWords(String words) {
+        String [] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
+
     public static void main (String[] args) {
         System.out.println("Starting Application");
         Application app = new Application();
         app.greet();
+        int count = app.countWords("I have four words") ;
+        System.out.println("Word count: " + count);
     }
 }
